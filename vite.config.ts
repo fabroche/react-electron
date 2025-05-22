902d3559d9a2c759825757be42a882a9fd1a5087
fabroche <kraxusmmo@gmail.com>
2025-05-22 19:01:02 +0100

    chore: DX Improved with npm-run-all package and refactored dev:* scripts.

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: './',
     build: {
         outDir: 'dist-react',
     },
-    base: './'
+    server: {
+        port: 5123,
+        strictPort: true
+    }
 })
