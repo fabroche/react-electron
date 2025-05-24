16bf04d01fe5e0d85f96aa86d1ed188bfe018377
fabroche <kraxusmmo@gmail.com>
2025-05-24 17:40:59 +0100

    feat: wrappers for ipcMain and ipcWebContents to implement type safety.

--- src/electron/util.ts
+++ src/electron/util.ts
@@ -1,7 +1,26 @@
+import {ipcMain, type WebContents} from "electron";
+
 function isDev(): boolean {
     return process.env.NODE_ENV === 'development';
 }
 
+function ipcMainHandle<Key extends keyof EventPayloadMapping, >(
+    key: Key,
+    handler: () => EventPayloadMapping[Key]
+) {
+    ipcMain.handle(key, () => handler())
+}
+
+function ipcWebContentsSend<Key extends keyof EventPayloadMapping>(
+    key: Key,
+    webContents: WebContents,
+    payload: EventPayloadMapping[Key],
+) {
+    webContents.send(key, payload)
+}
+
 export {
-    isDev
+    isDev,
+    ipcMainHandle,
+    ipcWebContentsSend
 };
